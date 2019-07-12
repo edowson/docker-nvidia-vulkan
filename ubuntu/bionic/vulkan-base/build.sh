0ba6a85aaf1bfd5a524a06b3d3d23ca716a0c8c6
Elvis Dowson <elvis.dowson@gmail.com>
2019-07-12 23:07:27 +0400

    ubuntu/bionic/vulkan-base: Fix ray-tracing support.
    Signed-off-by: Elvis Dowson <elvis.dowson@gmail.com>

--- ubuntu/bionic/vulkan-base/build.sh
+++ ubuntu/bionic/vulkan-base/build.sh
@@ -9,10 +9,11 @@ BUILD_VERSION=${1:-"1.1.108.0"}
 CUDA_MAJOR_VERSION='10.1'
 CUDNN_VERSION='7.6.1.34'
 CONDA_PYTHON_VERSION='3'
-CONDA_BASE_PACKAGE='anaconda'
-CONDA_VERSION='2019.03'
+CONDA_BASE_PACKAGE='miniconda'
+CONDA_VERSION='4.6.14'
 LLVM_VERSION='8'
-NVIDIA_DRIVER_VERSION='430.14'
+NVIDIA_DRIVER_VERSION='430.26'
+VULKAN_SDK_VERSION="$BUILD_VERSION"
 USER='developer'
 USER_ID='1000'
 CODE_NAME='bionic'
@@ -21,7 +22,9 @@ OPTION=""
 
 : '
 Information about available software versions:
-01. LLVM: 5.0, 6.0, 7, 8.
+01. anaconda: 2019.03
+02. miniconda: 4.6.14
+03. LLVM: 5.0, 6.0, 7, 8.
 '
 
 # use tar to dereference the symbolic links from the current directory,
@@ -29,6 +32,8 @@ Information about available software versions:
 tar -czh . | docker build - \
   --build-arg REPOSITORY=nvidia/cudagl \
   --build-arg TAG="$CUDA_MAJOR_VERSION-devel-ubuntu18.04" \
+  --build-arg BUILD_VERSION=$BUILD_VERSION \
+  --build-arg BUILD_DATE=$BUILD_DATE \
   --build-arg CUDA_MAJOR_VERSION=$CUDA_MAJOR_VERSION \
   --build-arg CUDNN_VERSION=$CUDNN_VERSION \
   --build-arg CONDA_PYTHON_VERSION=$CONDA_PYTHON_VERSION \
@@ -36,8 +41,7 @@ tar -czh . | docker build - \
   --build-arg CONDA_VERSION=$CONDA_VERSION \
   --build-arg LLVM_VERSION=$LLVM_VERSION \
   --build-arg NVIDIA_DRIVER_VERSION=$NVIDIA_DRIVER_VERSION \
-  --build-arg BUILD_VERSION=$BUILD_VERSION \
-  --build-arg BUILD_DATE=$BUILD_DATE \
+  --build-arg VULKAN_SDK_VERSION=$VULKAN_SDK_VERSION \
   --build-arg USER=$USER \
   --build-arg UID=$USER_ID \
   --tag=${REPOSITORY}:${TAG}
